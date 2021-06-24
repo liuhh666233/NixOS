1dfb3d6931d219883ae488f50a4dea2831569da7
liuhh666233 <liuxiaobo666233@gmail.com>
2021-06-25 01:47:53 +0800

    Add duf free command path /bin/free /bin/duf

--- cpu_info/app.py
+++ cpu_info/app.py
@@ -5,17 +5,15 @@ import click
 def cpu_info_check(free_type = "m", interval = 5):
     while True:
         print(time.ctime())
-        os.system("duf")
-        os.system(f"free -{free_type}")
+        os.system("/bin/duf")
+        os.system(f"/bin/free -{free_type}")
         time.sleep(interval)
 
 @click.command()
 @click.option('free_type', '--f', type = click.STRING,
                 help = "The free_type will asign to free -free_type")
-@click.option('interval', '--i',
-              type = click.INT,
+@click.option('interval', '--i',type = click.INT,
               help = 'Greet the person every interval seconds.')
-
 def main(free_type, interval):
     cpu_info_check(free_type, interval)
 
